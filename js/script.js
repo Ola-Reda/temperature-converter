e88c339cc5e8e055880f5d2cdb75d75ccf888e3a
Ola-Reda <ola.reda.900@gmail.com>
2023-11-25 00:04:12 +0200

    Add final changes

--- js/script.js
+++ js/script.js
@@ -16,20 +16,41 @@ function displayTime() {
     } else {
         session = "AM"
     }
-    time.innerHTML = `${hours}.${minutes}${session}`
+    time.innerHTML = `${hours}.${minutes} ${session}`
 }
 setInterval(displayTime, 10)
 
 //function convert
-convertBtn.addEventListener("click", convert) 
+convertBtn.addEventListener("click", () => {
+    convert()
+    convertBtn.innerHTML = `<i class="fa-solid fa-spinner"></i>   converting ...`
+    setTimeout(()=> {
+        convertBtn.innerHTML = `Convert`
+    },1000)
+}) 
+
+// if(inputNumber.value === '') {
+//     convertBtn.setAttribute("disabled", "")
+// } 
+
+// setTimeout(() => {
+//     convertBtn.removeAttribute("disabled")
+// }, 5000)
+
+
+
+
 function convert() {
-    let value = inputNumber.value
-    if(value !== ''){
-        if(type.value === 'Fahrenheit') {
-            degree = (value - 32) / 1.8
-        } else {
-            degree = value - 273.15
+    setTimeout(() => {
+        let value = inputNumber.value
+        if(value !== '') {
+            if(type.value === 'Fahrenheit') {
+                degree = (value - 32) / 1.8
+                result.innerHTML = `${degree.toFixed(2)} &deg;c`
+            } else {
+                degree = value - 273.15
+                result.innerHTML = `${degree.toFixed(2)} &deg;c`
+            }
         }
-    }
-    result.innerHTML = `${degree.toFixed(2)} &deg;c`
+    },1200)
 }
